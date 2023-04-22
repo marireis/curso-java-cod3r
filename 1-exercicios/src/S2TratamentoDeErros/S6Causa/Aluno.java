5a3f2a24cc4d5511e05f37ff47ed264c0716932d
Marina Reis <marinareis.mr@gmail.com>
2023-04-22 13:43:07 -0300

    Causa

---
+++ 1-exercicios/src/S2TratamentoDeErros/S6Causa/Aluno.java
@@ -0,0 +1,48 @@
+package S2TratamentoDeErros.S6Causa;
+
+import java.util.Objects;
+
+public class Aluno {
+
+	public String nome;
+	public double nota;
+	public boolean bomComportamento;
+	
+	//chama o constutor de baixo com this e passa bomCOmportamento como true;
+	public Aluno(String nome, double nota) {
+		this(nome, nota,true);	
+	}
+	
+	public Aluno(String nome, double nota, boolean bomComportamento) {
+		
+		this.nome = nome;
+		this.nota = nota;
+		this.bomComportamento = bomComportamento;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", nota=" + nota + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(bomComportamento, nome, nota);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		return bomComportamento == other.bomComportamento && Objects.equals(nome, other.nome)
+				&& Double.doubleToLongBits(nota) == Double.doubleToLongBits(other.nota);
+	}
+	
+	
+	
+}
---
+++ 1-exercicios/src/S2TratamentoDeErros/S6Causa/S6Causa.java
@@ -0,0 +1,35 @@
+package S2TratamentoDeErros.S6Causa;
+
+public class S6Causa {
+	
+	public static void main(String[] args) {
+		try {
+			metodoA(null);
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			if(e.getCause() != null) {
+				System.out.println(e.getCause().getMessage());
+			}
+		}
+		
+		metodoA(null);
+		
+	}
+	
+	static void metodoA(Aluno aluno) {
+		try {
+			metodoB(aluno);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			throw new IllegalArgumentException(e);
+		}
+	}
+	
+	static void metodoB(Aluno aluno) {
+		if(aluno == null) {
+		throw new NullPointerException("Aluno está nulo ");
+		}
+		System.out.println(aluno.nome);
+	}
+
+}
